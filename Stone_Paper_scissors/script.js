54da8ad46b45d77909be769ea76e33502b10e214
Bhavesh_707 <bhaveshgadling2025@gmail.com>
2025-05-05 18:01:38 +0530

    My First Project

---
+++ Stone_Paper_scissors/script.js
@@ -0,0 +1,89 @@
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll('.choice')
+const msg = document.querySelector('#msg')
+const compScoreTracker = document.querySelector('#comp')
+const userScoreTracker = document.querySelector('#user')
+const resetbtn = document.querySelector('#reset-btn')
+let userChoice, computerChoice;
+let gameChoice = ['Rock', 'Paper', 'Scissors']
+choices.forEach(function(choice) {
+    console.log(choice)
+    choice.addEventListener( 'click' ,function() {
+       let choiceId = choice.getAttribute('id')
+       console.log(choiceId)
+       countChoice(choiceId)
+    })
+})
+
+function countChoice(choiceId) {
+    if(choiceId == 'rock')
+        userChoice = 0;
+    else if(choiceId == 'paper')
+        userChoice = 1
+    else
+        userChoice = 2
+    // console.log(userChoice)
+    computerMove(userChoice);    
+}
+
+function computerMove(userChoice) {
+    computerChoice = Math.floor(Math.random() * 3)
+    // console.log(computerChoice)
+    playGame(userChoice, computerChoice)
+}
+function showWinner(userWin) {
+    if(userWin) {
+        // console.log("You won!")
+         msg.style.backgroundColor = "green"
+         userScore++;
+        userScoreTracker.innerText = `${userScore}`
+        msg.innerText = `You Won! Your ${gameChoice[userChoice]} beats ${gameChoice[computerChoice]}.`
+    } else {
+        msg.innerText = `You Lost. ${gameChoice[computerChoice]} beats your ${gameChoice[userChoice]}`
+        compScore++;
+        compScoreTracker.innerText = `${compScore}`
+        
+         msg.style.backgroundColor = "red"
+        // console.log("Comp won!")
+    }
+
+}
+
+function drawGame() {
+    msg.innerText = "Game was draw. Play again"
+    msg.style.backgroundColor = "orange"
+    // console.log("Game was draw")
+}
+function playGame(userChoice, computerChoice) {
+    if (userChoice == computerChoice){
+        drawGame()
+    } else {
+        let userWin = true 
+        if(userChoice === 0) {
+            //scissors, paper
+             userWin = computerChoice === 1 ? false : true
+
+        } else if (userChoice === 1) {
+            //rock, scissors
+            userWin = computerChoice === 2 ? false : true
+
+        } else {
+            //rock, paper
+            userWin = computerChoice == 0 ? false : true
+        }
+        showWinner(userWin);
+    }
+}
+function resetGame () {
+    userScore = 0;
+    compScore = 0;
+    msg.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+    msg.innerText = "Play Your Move"
+    userScoreTracker.innerText = '0'
+    compScoreTracker.innerText = '0'
+}
+resetbtn.addEventListener('click', resetGame)
+//if u-> 0 c-> 1 -> c wins
+//if u-> 1 c -> 2 -> c wins
+//if u -> 2 c -> 0
---
+++ Tic_Toc_Toe/script.js
@@ -0,0 +1,110 @@
+let boxes = document.querySelectorAll('.box')
+let reset_btn = document.querySelector('#reset-btn')
+let newGamebtn = document.querySelector('#new-btn')
+let msgContainer = document.querySelector('.msg-container')
+let playerTurn = document.querySelector('#turns_track')
+let msg = document.querySelector('#msg')
+let turn0 = true; //tracking the turn of the player
+
+let wining_patterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
+];
+//checking if box is empty
+boxes.forEach((box) => {
+  //adding a event listerner if the box is click
+  box.addEventListener('click', () => {
+    console.log('box is clicked')
+    if (turn0) {
+      playerTurn.innerText = `Player X Turn`
+      box.style.color = 'blue'
+      box.innerHTML = 'O'
+
+      turn0 = false
+    } else {
+      turn0 = true;
+      playerTurn.innerText = `Player O Turn`
+      box.style.color = 'black'
+      box.innerHTML = 'X'
+      
+    }
+    box.disabled = true
+    checkwinner();
+  })
+})
+
+//check winner is found after clicking it
+const checkwinner = function () {
+  let winnerFound = false;
+
+  for (let patterns of wining_patterns) {
+    let pos1 = boxes[patterns[0]].innerText
+    let pos2 = boxes[patterns[1]].innerText
+    let pos3 = boxes[patterns[2]].innerText;
+
+    if (pos1 != '' && pos2 != '' && pos3 != '') {
+      if (pos1 === pos2 && pos2 === pos3) {
+        showWinner(pos1);
+        winnerFound = true;
+        return;
+      }
+    }
+  }
+  //if winner is not found
+  if (!winnerFound) {
+    checkDraw();
+  }
+}
+
+function checkDraw() {
+  let filled = true;
+
+  boxes.forEach((box) => {
+    if (box.innerText === '') {
+      filled = false;
+    }
+  });
+
+  if (filled) {
+    msg.innerText = "It's a Draw!";
+    playerTurn.classList.add('hide')
+    msgContainer.classList.remove('hide');
+  }
+}
+
+function showWinner(winner) {
+  msg.innerText = `Congrats Winner is player ${winner}!`
+  msgContainer.classList.remove('hide');
+  playerTurn.classList.add('hide')
+  disableBoxes();
+}
+
+function disableBoxes() {
+  for (let box of boxes) {
+    box.disabled = true
+  }
+}
+
+function enableBoxes() {
+  for (let box of boxes) {
+    box.disabled = false
+    box.innerText = ''
+    playerTurn.classList.remove('hide')
+    msgContainer.classList.add('hide');
+  }
+}
+//reset the whole game
+function resetGame() {
+  turn0 = true
+  playerTurn.innerText = `Player O Turn`
+  enableBoxes()
+}
+
+newGamebtn.addEventListener('click', resetGame)
+reset_btn.addEventListener('click', resetGame)
